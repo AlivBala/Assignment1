6174617bed0cc857e91804ea56d5a4639e2a0326
Bala Aliveli <bala.aliveli@gmail.com>
2021-07-13 22:14:22 -0400

    uadding sample test

---
+++ API/inst.sh
@@ -0,0 +1,4 @@
+# install before run the test
+npm install -g @angular/cli
+npm install
+node_module/.bin/webdriver-manager update
---
+++ API/specs/API-Test.ts
@@ -0,0 +1,23 @@
+import { browser } from "protractor";
+var frisby = require('frisby');
+var usercount = 0;
+var expecing_UserNames = ['Darth Vader', 'Chewbacca', 'Roos Tarpals', 'Rugor Nass', 'Yarael Poof',
+    'Lama Su', 'Tuan Wu', 'Grievous', 'Tarfful', 'Tion Medon']
+describe("Validate the Star wars API", function () {
+    it('get the User details from Star wars API', function (done) {
+        frisby.get('https://swapi.dev/api/people')
+            .then(function (res) {
+                expect(res.status).toEqual(200, { "emsg": res.status + "expected to be 200", "estack": res._body })
+                console.log("details", JSON.stringify(res._body, null, 2))
+
+                for (var i = 0; i < res._body.length; i++) {
+                    if (res._body.results[i].height > 200) {
+                        usercount = res._body.results[i].length
+                        break;
+                    }
+                }
+                expect(usercount).toBe(10)
+            })
+            .done(done)
+    });
+})
