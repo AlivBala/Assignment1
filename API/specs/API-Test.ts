649506edf280524f0998f8ffe4c1d6b083c814b6
Bala Aliveli <bala.aliveli@gmail.com>
2021-07-14 09:21:57 -0400

    updated framework

--- API/specs/API-Test.ts
+++ API/specs/API-Test.ts
@@ -1,23 +1,24 @@
 import { browser } from "protractor";
 var frisby = require('frisby');
 var usercount = 0;
-var expecing_UserNames = ['Darth Vader', 'Chewbacca', 'Roos Tarpals', 'Rugor Nass', 'Yarael Poof',
-    'Lama Su', 'Tuan Wu', 'Grievous', 'Tarfful', 'Tion Medon']
+var userNames = [];
+var expecing_UserNames = ['Chewbacca', 'Darth Vader', 'Grievous', 'Lama Su',
+    'Roos Tarpals', 'Rugor Nass', 'Tuan Wu', 'Tarfful', 'Tion Medon', 'Yarael Poof']
 
 describe("Validate the Star wars API", function () {
     it('get the User details from Star wars API', function (done) {
         frisby.get('https://swapi.dev/api/people')
             .then(function (res) {
                 expect(res.status).toEqual(200, { "emsg": res.status + "expected to be 200", "estack": res._body })
-                console.log("details", JSON.stringify(res._body, null, 2))
-
                 for (var i = 0; i < res._body.results.length; i++) {
                     if (res._body.results[i].height > 200) {
                         usercount = res._body.results.length
-                        break;
+                        userNames.push(res._body.results[i].name)
+                        userNames.sort()
                     }
                 }
-                expect(usercount).toBe(10)
+                expect(usercount).toBe(10, { "emsg": "Users count:" + usercount + "expected to be 10", "estack": res._body })
+                expect(userNames).toContain(expecing_UserNames, { "emsg": "Usernames: " + userNames + "should be match with : " + expecing_UserNames, "estack": res._body })
             })
             .done(done)
     });
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Assignment1
+# this one have 2 different frame works for UI (Protractor and Cucumber) and API (Protractor and Jasmine)
