fa7661fe4a45168ce5839a119c2ed0a9677884fe
Bala Aliveli <bala.aliveli@gmail.com>
2021-07-13 23:02:14 -0400

    update for loop

--- API/specs/API-Test.ts
+++ API/specs/API-Test.ts
@@ -3,7 +3,7 @@ var frisby = require('frisby');
 var usercount = 0;
 var expecing_UserNames = ['Darth Vader', 'Chewbacca', 'Roos Tarpals', 'Rugor Nass', 'Yarael Poof',
     'Lama Su', 'Tuan Wu', 'Grievous', 'Tarfful', 'Tion Medon']
-    
+
 describe("Validate the Star wars API", function () {
     it('get the User details from Star wars API', function (done) {
         frisby.get('https://swapi.dev/api/people')
@@ -11,9 +11,9 @@ describe("Validate the Star wars API", function () {
                 expect(res.status).toEqual(200, { "emsg": res.status + "expected to be 200", "estack": res._body })
                 console.log("details", JSON.stringify(res._body, null, 2))
 
-                for (var i = 0; i < res._body.length; i++) {
+                for (var i = 0; i < res._body.results.length; i++) {
                     if (res._body.results[i].height > 200) {
-                        usercount = res._body.results[i].length
+                        usercount = res._body.results.length
                         break;
                     }
                 }
